da211f167da80dd9e27c0cded966cd76a79abac0
AndreaPiriz <68929750+AndreaPiriz@users.noreply.github.com>
2020-09-04 18:02:55 -0300

    Se agregó un buscador, además en cada producto se agregó un link que lleva a la información del mismo

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,28 @@
+var products = {};
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+
+            let productName        = document.getElementById("productName");
+            let productDescription = document.getElementById("productDescription");
+            let productCost        = document.getElementById("productCost");
+            let productCurrency    = document.getElementById("productCurrency");
+            let productCategory    = document.getElementById("productCategory");
+
+            productName.innerHTML         = product.name;
+            productDescription.innerHTML  = product.description;
+            productCost.innerHTML         = product.cost;
+            productCurrency.innerHTML     = product.currency;
+            productCategory.innerHTML     = product.category;
+
+            
+        }
+    });
 
 });
